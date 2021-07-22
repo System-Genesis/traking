a1bd911029f5a10ae672004f2be4f21654967b08
Roei <roeioren10@gmail.com>
2021-07-22 16:37:25 +0300

    fix volumes dist

--- volumes/mergesAPI.sources.js
+++ volumes/mergesAPI.sources.js
@@ -1,1 +1,3 @@
-export default ['aka', 'es_name', 'adNN_name', 'sf_name', 'city_name', 'mir_name'];
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ['aka', 'es_name', 'adNN_name', 'sf_name', 'city_name', 'mir_name'];
+//# sourceMappingURL=sources.js.map
