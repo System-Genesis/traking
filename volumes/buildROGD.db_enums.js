636328f3041eb95d819fea380c147839408fb7d5
Roei <roeioren10@gmail.com>
2021-07-22 11:17:08 +0300

    add .gitignore, add volumes to basicMatch, mergesAPI and buildROGD

---
+++ volumes/buildROGD.db_enums.js
@@ -0,0 +1,9 @@
+import fieldNames from './fieldNames';
+export const ROLE_ID_SUFFIXES = [
+    [fieldNames.sources.ads, '@rabiran'],
+    [fieldNames.sources.es, '@jello'],
+    [fieldNames.sources.adNN, '@adnn'],
+    [fieldNames.sources.sf, '@leonardo'],
+    [fieldNames.sources.city, '@city'],
+];
+
